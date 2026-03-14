9941a5a4c3432044efab91b86203eb1ac5b57ced
luckysharma824 <abhisheksharma7j@gmail.com>
2026-03-14 17:32:00 +0530

    added token

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
+import base64
 
 load_dotenv()
 
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+_token_b64 = os.getenv("GITHUB_TOKEN_B64")
+GITHUB_TOKEN = base64.b64decode(_token_b64).decode("utf-8") if _token_b64 else None
 GITHUB_REPO = os.getenv("GITHUB_REPO")
